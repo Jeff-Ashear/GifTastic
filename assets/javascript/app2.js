cbd351478d1e2cac5f9006ccbc54345c95fd2896
Jeff.A <kinesthesiac@gmail.com>
2020-01-16 18:15:24 -0700

    got the api key and coded an ajax function, but it doesn't quite work yet.

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -9,6 +9,7 @@ var topics = [
     "Joan Lambert",
     "Big Chap",
     "Xenomorph",
+    "a bean",
 
 ];
 
@@ -23,6 +24,11 @@ $("button").on("click", function() {
     var thisGIF = $(this).attr("data-gif");
     console.log("click")
     console.log(thisGIF)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisGIF + "&api_key=8S6m8Ed7pFQS1y8sTnybvoCJPvAbRWYW&limit=10";
+
+    $.ajax({
+        queryURL
+    })
 
 
 })
