4362769a66769d772d39db5a1eb5c4fffe7c35c6
Jeff.A <kinesthesiac@gmail.com>
2020-04-04 20:51:11 -0700

    pushing again to preserve the functional state of the favorite me button.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GifTastic
+
+
+innerHTML: "<img class="gif" data-state="still" src="https://media2.giphy.com/media/l1IYgGkUFlyHWZdqE/200_s.gif?cid=36ba7033bb733bdfadec5608f8202313c29b05785c11f437&amp;rid=200_s.gif" data-still="https://media2.giphy.com/media/l1IYgGkUFlyHWZdqE/200_s.gif?cid=36ba7033bb733bdfadec5608f8202313c29b05785c11f437&amp;rid=200_s.gif" data-animated="https://media2.giphy.com/media/l1IYgGkUFlyHWZdqE/200.gif?cid=36ba7033bb733bdfadec5608f8202313c29b05785c11f437&amp;rid=200.gif"><p>Rating: g  </p><button id="favoriteBtn">Favorite Me</button><div></div>"
--- assets/javascript/app5.js
+++ assets/javascript/app5.js
@@ -28,72 +28,92 @@ for (var i = 0; i < topics.length; i++) {
 };
 
 //clicking a button catches the value of that button
-$(document).on("click", ".gifBtn", function() {
+$(document).on("click", ".gifBtn", function () {
     var thisGIF = $(this).attr("data-gif");
     console.log("click");
     console.log("thisGIF is now: ", thisGIF);
     //inserts the value of the button clicked into a giphy search query
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisGIF + "&api_key=8S6m8Ed7pFQS1y8sTnybvoCJPvAbRWYW&limit=10";
-    
+
     //ajax promise
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-    .then(function(response){
-        var results = response.data;
-        console.log(response)
-        
-        //iterating the rusults of the query, and grabbing the appropriate data to append to the html
-        for (var j = 0; j < results.length; j++) {
-            
-            var gifDiv = $('<div id="gifDiv">');
-            
-            var rating = results[j].rating;
-            
-            var p = $('<p>').text('Rating: ' + rating + "  ");
+        .then(function (response) {
+            var results = response.data;
+            console.log(response)
 
-            var favBtn = $('<button id="favoriteBtn">Favorite Me</button>');
-            
-            var gifStart = $('<img class="gif" data-state="still">');
-            gifStart.attr("src", results[j].images.fixed_height_still.url); gifStart.attr("data-still", results[j].images.fixed_height_still.url);
-            gifStart.attr("data-animated", results[j].images.fixed_height.url);
-            
-            gifDiv.append(gifStart);
-            console.log("gifDiv is happened on ", thisGIF);
-            gifDiv.append(p);
-            console.log("appending P happened on ", thisGIF);
-            // favBtn.appendTo("#controls");
-            gifDiv.append(favBtn);
-            console.log("appending favBtn happened on ", thisGIF);
+            //iterating the rusults of the query, and grabbing the appropriate data to append to the html
+            for (var j = 0; j < results.length; j++) {
 
-            $("#gif-Feed").prepend(gifDiv);
-            console.log(gifDiv)
-            
-        } 
-        //function to start and stop animations
-        $(".gif").on("click", function() {
-            
-            console.log("CLICKED GIF");
-            console.log(this)
-            var state = $(this).attr("data-state");
-            
-            if (state === "still") {
-                var zTHisIs = $(this).attr("src", $(this).attr("data-animated"));
-                $(this).attr("src", $(this).attr("data-animated"));
-                $(this).attr("data-state", "animate");
-                console.log("this again: " + this)
-                console.log("This is zTHisIs alright: ", zTHisIs);
-            } else if (state === "animate") { 
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-            }   
-        });   
-    });   
+                var gifDiv = $('<div id="gifDiv">');
+
+                var rating = results[j].rating;
+
+                var p = $('<p>').text('Rating: ' + rating + "  ");
+
+                var favBtn = $('<button id="favoriteBtn">Favorite Me</button>');
+
+                var gifStart = $('<img class="gif" data-state="still">');
+                gifStart.attr("src", results[j].images.fixed_height_still.url); gifStart.attr("data-still", results[j].images.fixed_height_still.url);
+                gifStart.attr("data-animated", results[j].images.fixed_height.url);
+
+                gifDiv.append(gifStart);
+                console.log("gifDiv is happened on ", thisGIF);
+                gifDiv.append(p);
+                console.log("appending P happened on ", thisGIF);
+                // favBtn.appendTo("#controls");
+                gifDiv.append(favBtn);
+                console.log("appending favBtn happened on ", thisGIF);
+
+                $("#gif-Feed").prepend(gifDiv);
+                console.log(gifDiv)
+
+            }
+
+            //When the favorite button is clicked, the appropiate gif appears in the favorites feed.
+            $(document).on("click", "#favoriteBtn", function (event) {
+                event.preventDefault;
+                // favoriteMe = $(this).find(".gif", $(this).attr("data-still"));
+                // favoriteMe = $(this).find("#gifDiv", $(this).attr("src"));
+                // favoriteMe = $(this).parent().attr("src");
+                var favoriteMe = $(this).parent("div");
+                // var favoriteMeToo = favoriteMe[0].innerHTML
+                // favoriteMeToo = favoriteMeToo.spilt('<button id="favoriteBtn">Favorite Me</button>');
+                // console.log("favoriteMeToo: ", favoriteMeToo);
+                // favoriteGif = $('<div id="gifDiv">' + favoriteMeToo + '</div>');
+                favoriteGif = $('<div id="gifDiv">' + favoriteMe[0].innerHTML + '</div>');
+                console.log("Favorite clicked! It was ", favoriteMe);
+                console.log("Here's hoping: ", favoriteGif);
+                $("#favoriteFeed").prepend(favoriteGif).html;
+
+            });
+
+
+            //function to start and stop animations
+            $(".gif").on("click", function () {
+
+                console.log("CLICKED GIF");
+                console.log(this)
+                var state = $(this).attr("data-state");
+
+                if (state === "still") {
+                    var zTHisIs = $(this).attr("src", $(this).attr("data-animated"));
+                    $(this).attr("src", $(this).attr("data-animated"));
+                    $(this).attr("data-state", "animate");
+                    console.log("this again: " + this)
+                    console.log("This is zTHisIs alright: ", zTHisIs);
+                } else if (state === "animate") {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
+        });
 });
 
 //when the submit button is clicked, the user input's value is added to a button, which is then added to the html
-$("#buttonSubmit").on("click", function(event) {
+$("#buttonSubmit").on("click", function (event) {
     event.preventDefault();
     console.log("form click")
     var input = $("#userInput").val().trim();
@@ -110,19 +130,4 @@ $("#buttonSubmit").on("click", function(event) {
     // };
     $("#userInput").val('');
     $("header").css("background-color", "green");
-});
-
-
-//When the favorite button is clicked, the appropiate gif appears in the favorites feed.
-$(document).on("click", "#favoriteBtn", function(event) {
-    event.preventDefault;
-    // favoriteMe = $(this).find(".gif", $(this).attr("data-still"));
-    // favoriteMe = $(this).find("#gifDiv", $(this).attr("src"));
-    // favoriteMe = $(this).parent().attr("src");
-    favoriteMe = $(this).parent("div");
-    favoriteGif = favoriteMe[0].innerHTML
-    console.log("Favorite clicked! It was ", favoriteMe);
-    console.log("Here's hoping: ", favoriteGif);
-    $("#favoriteFeed").prepend(favoriteGif).html;
-
 });
